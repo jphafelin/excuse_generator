b914abaddd379b832050af5d7420a5dc98832722
jphafelin <josepablo.hafelin@gmail.com>
2023-01-11 19:29:42 +0000

    entrega generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His dragon", "My bird"];
+  let whoIcon = [
+    '<i class="fa-solid fa-dog"></i>',
+    "My grandma",
+    '<i class="fa-solid fa-dragon"></i>',
+    '<i class="fa-solid fa-crow"></i>',
+  ];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let actionRandom = Math.floor(Math.random() * action.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  return (
+    whoIcon[whoRandom] +
+    " " +
+    who[whoRandom] +
+    " " +
+    action[actionRandom] +
+    " " +
+    what[whatRandom] +
+    " " +
+    when[whenRandom]
+  );
+}
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
 };
